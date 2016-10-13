32539aeb39163b52c6c7348dce83704d976e6343
unloop <pastor.konstantin@gmail.com>
2016-10-13 15:11:29 +0300

    Changelog:   - run daemon service;   - read config file;   - init context;   - run http_server;   - corse patch;   - route get version;   - include logger;   - change packages;   - print help info;   - print version info.

--- cmd/daemon/daemon.go
+++ cmd/daemon/daemon.go
@@ -1,1 +1,67 @@
 package daemon
+
+import (
+	"github.com/deployithq/deployit/cmd/daemon/config"
+	"github.com/deployithq/deployit/cmd/daemon/context"
+	"github.com/deployithq/deployit/libs/log"
+	"github.com/jawher/mow.cli"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Run(cmd *cli.Cmd) {
+
+	var ctx = context.Get()
+	var cfg = config.Get()
+
+	cmd.Spec = "[-c][-d]"
+
+	var debug = cmd.Bool(cli.BoolOpt{Name: "d debug", Value: false, Desc: "Enable debug mode"})
+	var configPath = cmd.String(cli.StringOpt{Name: "c config", Value: "./config.yaml", Desc: "Path to config file", HideValue: true})
+
+	cmd.Before = func() {
+
+		ctx.Log = new(log.Log)
+		ctx.Log.Init()
+
+		if *configPath != "" {
+
+			// Parsing config file
+			configBytes, err := ioutil.ReadFile(*configPath)
+			if err != nil {
+				ctx.Log.Panic(err)
+			}
+
+			err = yaml.Unmarshal(configBytes, cfg)
+			if err != nil {
+				ctx.Log.Panic(err)
+			}
+		}
+
+		if *debug {
+			cfg.Debug = *debug
+			ctx.Log.SetDebugLevel()
+			ctx.Log.Info("Logger debug mode enabled")
+		}
+
+		if cfg.HttpServer.Port == 0 {
+			cfg.HttpServer.Port = 3000
+		}
+	}
+
+	cmd.Action = func() {
+		ctx.Log.Info("Initializing daemon")
+
+		go RunHttpServer(cfg.HttpServer.Port)
+
+		// Handle SIGINT and SIGTERM.
+		ch := make(chan os.Signal)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		ctx.Log.Debug(<-ch)
+
+		ctx.Log.Info("Handle SIGINT and SIGTERM.")
+	}
+}
--- libs/log/log.go
+++ libs/log/log.go
@@ -1,1 +1,135 @@
 package log
+
+import (
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"os"
+	"runtime"
+)
+
+type Log struct {
+	logger *logrus.Logger
+	skip   int
+}
+
+func (l *Log) Init() {
+	l.logger = logrus.New()
+	l.logger.Out = os.Stdout
+	l.skip = 2
+}
+
+func (l *Log) SetDebugLevel() {
+	l.logger.Level = logrus.DebugLevel
+}
+
+func (l *Log) Debug(args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Debug(args)
+	}
+}
+
+func (l *Log) Debugf(format string, args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Debugf(format, args)
+	}
+}
+
+func (l *Log) Info(args ...interface{}) {
+	if l.logger.Level >= logrus.InfoLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Info(args...)
+	}
+}
+
+func (l *Log) Infof(format string, args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Infof(format, args)
+	}
+}
+
+func (l *Log) Error(args ...interface{}) {
+	if l.logger.Level >= logrus.ErrorLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Error(args...)
+	}
+}
+
+func (l *Log) Errorf(format string, args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Errorf(format, args)
+	}
+}
+
+func (l *Log) Fatal(args ...interface{}) {
+	if l.logger.Level >= logrus.FatalLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Fatal(args...)
+	}
+}
+
+func (l *Log) Fatalf(format string, args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Fatalf(format, args)
+	}
+}
+
+func (l *Log) Panic(args ...interface{}) {
+	if l.logger.Level >= logrus.PanicLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Panic(args...)
+	}
+}
+
+func (l *Log) Panicf(format string, args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Panicf(format, args)
+	}
+}
+
+func (l *Log) Warn(args ...interface{}) {
+	if l.logger.Level >= logrus.WarnLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Warn(args...)
+	}
+}
+
+func (l *Log) Warnf(format string, args ...interface{}) {
+	if l.logger.Level >= logrus.DebugLevel {
+		entry := l.logger.WithFields(logrus.Fields{})
+		entry.Data["file"] = fileLine(l.skip)
+		entry.Warnf(format, args)
+	}
+}
+
+// Logger for GORM
+func (l *Log) Print(args ...interface{}) {
+	if l.logger.Level >= logrus.ErrorLevel {
+		l.logger.Error(args)
+	}
+}
+
+func fileLine(skip int) string {
+	_, file, line, ok := runtime.Caller(skip)
+	if !ok {
+		file = "<???>"
+		line = 1
+	}
+	return fmt.Sprintf("%s:%d", file, line)
+}
