3e9908e6871e363c5d3e07fabb74a23b738029e0
Konstantin Pastor <pastor.konstantin@gmail.com>
2016-10-21 20:51:27 +0300

    update log library

--- libs/log/log.go
+++ libs/log/log.go
@@ -2,127 +2,85 @@ package log
 
 import (
 	"fmt"
-	"github.com/Sirupsen/logrus"
-	"os"
+	"github.com/deployithq/deployit/libs/log/color"
 	"runtime"
+	"time"
 )
 
+const _NEWLINE = "\n"
+
 type Log struct {
-	logger *logrus.Logger
-	skip   int
+	skip  int
+	debug bool
 }
 
 func (l *Log) Init() {
-	l.logger = logrus.New()
-	l.logger.Out = os.Stdout
 	l.skip = 2
 }
 
 func (l *Log) SetDebugLevel() {
-	l.logger.Level = logrus.DebugLevel
+	l.debug = true
 }
 
 func (l *Log) Debug(args ...interface{}) {
-	if l.logger.Level >= logrus.DebugLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Debug(args)
+	if l.debug {
+		l.print(color.White(l.sprintlnn(args...)))
 	}
 }
 
 func (l *Log) Debugf(format string, args ...interface{}) {
-	if l.logger.Level >= logrus.DebugLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Debugf(format, args)
+	if l.debug {
+		l.printf(color.White(format), args...)
 	}
 }
 
 func (l *Log) Info(args ...interface{}) {
-	if l.logger.Level >= logrus.InfoLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Info(args...)
-	}
+	l.print(color.Yellow(l.sprintlnn(args...)))
 }
 
 func (l *Log) Infof(format string, args ...interface{}) {
-	if l.logger.Level >= logrus.InfoLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Infof(format, args)
-	}
+	l.printf(color.Yellow(format), args...)
 }
 
 func (l *Log) Error(args ...interface{}) {
-	if l.logger.Level >= logrus.ErrorLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Error(args...)
-	}
+	l.print(color.Red(l.sprintlnn(args...)))
 }
 
 func (l *Log) Errorf(format string, args ...interface{}) {
-	if l.logger.Level >= logrus.ErrorLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Errorf(format, args)
-	}
+	l.printf(color.Red(format), args...)
 }
 
 func (l *Log) Fatal(args ...interface{}) {
-	if l.logger.Level >= logrus.FatalLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Fatal(args...)
-	}
+	l.print(color.Red(l.sprintlnn(args...)))
 }
 
 func (l *Log) Fatalf(format string, args ...interface{}) {
-	if l.logger.Level >= logrus.FatalLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Fatalf(format, args)
-	}
+	l.printf(color.Red(format), args...)
 }
 
 func (l *Log) Panic(args ...interface{}) {
-	if l.logger.Level >= logrus.PanicLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Panic(args...)
-	}
+	l.print(color.Red(l.sprintlnn(args...)))
 }
 
 func (l *Log) Panicf(format string, args ...interface{}) {
-	if l.logger.Level >= logrus.PanicLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Panicf(format, args)
-	}
+	l.printf(color.Red(format), args...)
 }
 
 func (l *Log) Warn(args ...interface{}) {
-	if l.logger.Level >= logrus.WarnLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Warn(args...)
-	}
+	l.print(color.Magenta(l.sprintlnn(args...)))
 }
 
 func (l *Log) Warnf(format string, args ...interface{}) {
-	if l.logger.Level >= logrus.WarnLevel {
-		entry := l.logger.WithFields(logrus.Fields{})
-		entry.Data["file"] = fileLine(l.skip)
-		entry.Warnf(format, args)
-	}
+	l.printf(color.Magenta(format), args...)
 }
 
-// Logger for GORM
-func (l *Log) Print(args ...interface{}) {
-	if l.logger.Level >= logrus.ErrorLevel {
-		l.logger.Error(args)
-	}
+func (l *Log) print(message string) {
+	fmt.Printf("%v %s", time.Now().Format("2006-01-02 15:04:05"), message+_NEWLINE)
+}
+
+func (l *Log) printf(format string, a ...interface{}) {
+	fmt.Printf("%v %s", time.Now().Format("2006-01-02 15:04:05"), color.Cyan(fileLine(l.skip)))
+	fmt.Printf(format+_NEWLINE, a...)
 }
 
 func fileLine(skip int) string {
@@ -132,4 +90,13 @@ func fileLine(skip int) string {
 		line = 1
 	}
 	return fmt.Sprintf("%s:%d", file, line)
+}
+
+// Sprintlnn => Sprint no newline. This is to get the behavior of how
+// fmt.Sprintln where spaces are always added between operands, regardless of
+// their type. Instead of vendoring the Sprintln implementation to spare a
+// string allocation, we do the simplest thing.
+func (l *Log) sprintlnn(args ...interface{}) string {
+	msg := fmt.Sprintln(args...)
+	return msg[:len(msg)-1]
 }
