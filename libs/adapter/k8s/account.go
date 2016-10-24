a8a6f2507f026c4172b0fdff4c53456fcdea5204
undassa <alexander@lastbackend.com>
2016-10-24 21:01:11 +0300

    Add build spec

--- libs/adapter/k8s/account.go
+++ libs/adapter/k8s/account.go
@@ -4,31 +4,31 @@ import (
 	account "github.com/lastbackend/lastbackend/libs/adapter/k8s/api/v1"
 )
 
-type ComponentAccountsGetter interface {
-	Accounts() ComponentAccountInterface
+type AccountsGetter interface {
+	Accounts() AccountInterface
 }
 
 // ComponentStatusInterface has methods to work with ComponentStatus resources.
-type ComponentAccountInterface interface {
+type AccountInterface interface {
 	Create(*account.Account) (*account.Account, error)
 }
 
 // componentStatuses implements ComponentStatusInterface
-type componentAccounts struct {
-	client *LBClient
+type Accounts struct {
+	c *LBClient
 }
 
 // newComponentStatuses returns a ComponentStatuses
-func newComponentAccounts(c *LBClient) *componentAccounts {
-	return &componentAccounts{
-		client: c,
+func newAccounts(c *LBClient) *Accounts {
+	return &Accounts{
+		c: c,
 	}
 }
 
 // Create takes the representation of a componentStatus and creates it.  Returns the server's representation of the componentStatus, and an error, if there is any.
-func (c *componentAccounts) Create(Account *account.Account) (result *account.Account, err error) {
+func (c *Accounts) Create(Account *account.Account) (result *account.Account, err error) {
 	result = &account.Account{}
-	err = c.client.Post().
+	err = c.c.Post().
 		Resource("accounts").
 		Body(Account).
 		Do().
