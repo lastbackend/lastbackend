6a014e5ef7e380514eeb5b7c613d1aaf84ece0cc
Konstantin Pastor <pastor.konstantin@gmail.com>
2016-11-22 21:09:53 +0300

    update vendoring. update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-echo "===> Test common libs:"
-go test ./libs/...
-echo "\n"
+#echo "===> Test common libs:"
+#go test ./libs/...
+#echo "\n"
+#
+#echo "===> Test common packages:"
+#go test ./pkg/...
+#echo "\n"#
 
 echo "===> Test common packages:"
-go test ./pkg/...
+go test -v $(go list ./... | grep -v /vendor/)
 echo "\n"
