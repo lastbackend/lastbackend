f2f9b38e727652e678a96692a56bd6091d59f2ec
junee455 <maichaelma455@gmail.com>
2016-11-26 12:45:43 +0300

    services updated

--- libs/model/service.go
+++ libs/model/service.go
@@ -1,1 +1,47 @@
 package model
+
+import (
+	"encoding/json"
+	e "github.com/lastbackend/lastbackend/libs/errors"
+	"time"
+)
+
+type ServiceList []Service
+
+type Service struct {
+	// Service uuid, incremented automatically
+	ID string `json:"id" gorethink:"id,omitempty"`
+	// Service user
+	User string `json:"user" gorethink:"user,omitempty"`
+	// Service image
+	Image string `json:"image" gorethink:"image,omitempty"`
+	// Service name
+	Name string `json:"name" gorethink:"name,omitempty"`
+	// Service created time
+	Created time.Time `json:"created" gorethink:"created,omitempty"`
+	// Service updated time
+	Updated time.Time `json:"updated" gorethink:"updated,omitempty"`
+}
+
+func (s *Service) ToJson() ([]byte, *e.Err) {
+	buf, err := json.Marshal(s)
+	if err != nil {
+		return nil, e.Service.Unknown(err)
+	}
+
+	return buf, nil
+}
+
+func (s *ServiceList) ToJson() ([]byte, *e.Err) {
+
+	if s == nil {
+		return []byte("[]"), nil
+	}
+
+	buf, err := json.Marshal(s)
+	if err != nil {
+		return nil, e.Service.Unknown(err)
+	}
+
+	return buf, nil
+}
--- pkg/client/cmd/service/service.go
+++ pkg/client/cmd/service/service.go
@@ -1,9 +1,76 @@
 package service
 
 import (
-	"fmt"
+	"github.com/lastbackend/lastbackend/libs/model"
+	"github.com/lastbackend/lastbackend/pkg/client/context"
+	e "github.com/lastbackend/lastbackend/libs/errors"
 )
 
-func Check() {
-	
+type serviceCreate struct {
+	Name string `json:"name"`
+
+}
+
+func getToken(ctx *context.Context) (string, error) {
+	var err error
+	token := struct {
+		Token string `json:"token"`
+	}{}
+	err = ctx.Storage.Get("session", &token)
+	return token.Token, err
+}
+
+
+func Create(name string) error {
+	var (
+		err error
+		ctx = context.Get()
+		token string
+		res model.Project
+	)
+	token, err = getToken(&ctx)
+	req_err := new(e.Http)
+	_, _, err = ctx.HTTP.
+		POST("/service").
+		AddHeader("Content-Type", "application/json").
+		AddHeader("Authorization", "Bearer " + token).
+		BodyJSON(serviceCreate{name}).
+		Request(&res, req_err)
+	if err != nil {}
+	return err
+
+}
+
+func Inspect(name string) error {
+	var (
+		err error
+		ctx = context.Get()
+		token string
+		res model.Project
+	)
+	token, err = getToken(&ctx)
+	req_err := new(e.Http)
+	_, _, err = ctx.HTTP.
+		GET("/service/" + name).
+		AddHeader("Authorization", "Bearer " + token).
+		Request(&res, req_err)
+	if err != nil {}
+	return err
+}
+
+func Remove(name string) error {
+	var (
+		err error
+		ctx = context.Get()
+		token string
+		res model.Project
+	)
+	token, err = getToken(&ctx)
+	req_err := new(e.Http)
+	_, _, err = ctx.HTTP.
+		DELETE("/service/" + name).
+		AddHeader("Authorization", "Bearer " + token).
+		Request(&res, req_err)
+	if err != nil {}
+	return err
 }
