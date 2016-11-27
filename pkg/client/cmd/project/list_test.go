83fad613c1e2c539aa1ac4811c57433f5e8d6ae9
Konstantin Pastor <pastor.konstantin@gmail.com>
2016-11-27 20:48:15 +0300

    fix tests for running to client

--- pkg/client/cmd/project/list_test.go
+++ pkg/client/cmd/project/list_test.go
@@ -1,46 +1,59 @@
 package project_test
 
 import (
-	//h "github.com/lastbackend/lastbackend/libs/http"
-	//"github.com/lastbackend/lastbackend/pkg/client/cmd/project"
-	//"github.com/lastbackend/lastbackend/pkg/client/context"
-	//"github.com/stretchr/testify/assert"
-	//"net/http"
-	//"net/http/httptest"
+	h "github.com/lastbackend/lastbackend/libs/http"
+	"github.com/lastbackend/lastbackend/pkg/client/cmd/project"
+	"github.com/lastbackend/lastbackend/pkg/client/context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestList_Success(t *testing.T) {
 
-	//const (
-	//	name        string = "project"
-	//	description string = "project describe"
-	//	token       string = "mocktoken"
-	//)
-	//
-	//var ctx = context.Mock()
-	//ctx.Session.Set(token)
-	//
-	//server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//
-	//	tk := r.Header.Get("Authorization")
-	//
-	//	assert.NotEmpty(t, tk, "token should be not empty")
-	//	assert.Equal(t, tk, "Bearer "+token, "they should be equal")
-	//
-	//	w.WriteHeader(200)
-	//	_, err := w.Write([]byte(`[{"id":"mock", "name":"` + name + `", "description":"` + description + `"}]`))
-	//	if err != nil {
-	//		t.Error(err)
-	//		return
-	//	}
-	//}))
-	//defer server.Close()
-	//
-	//ctx.HTTP = h.New(server.URL)
-	//
-	//err := project.List()
-	//if err != nil {
-	//	t.Error(err)
-	//}
+	const (
+		name        string = "project"
+		description string = "project describe"
+		token       string = "mocktoken"
+	)
+
+  var (
+    err error
+    ctx = context.Mock()
+  )
+
+  err = ctx.Storage.Init()
+  if err != nil {
+    panic(err)
+  }
+  defer ctx.Storage.Close()
+
+	session := struct {
+		Token string `json:"token"`
+	}{token}
+	ctx.Storage.Set("session", session)
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		tk := r.Header.Get("Authorization")
+
+		assert.NotEmpty(t, tk, "token should be not empty")
+		assert.Equal(t, tk, "Bearer "+token, "they should be equal")
+
+		w.WriteHeader(200)
+		_, err := w.Write([]byte(`[{"id":"mock", "name":"` + name + `", "description":"` + description + `"}]`))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}))
+	defer server.Close()
+
+	ctx.HTTP = h.New(server.URL)
+
+	err = project.List()
+	if err != nil {
+		t.Error(err)
+	}
 }
--- pkg/client/cmd/user/whoami_test.go
+++ pkg/client/cmd/user/whoami_test.go
@@ -1,52 +1,61 @@
 package user_test
 
 import (
-	//"fmt"
-	//h "github.com/lastbackend/lastbackend/libs/http"
-	//"github.com/lastbackend/lastbackend/pkg/client/cmd/user"
-	//"github.com/lastbackend/lastbackend/pkg/client/context"
-	//"github.com/stretchr/testify/assert"
-	//"net/http"
-	//"net/http/httptest"
+	"fmt"
+	h "github.com/lastbackend/lastbackend/libs/http"
+	"github.com/lastbackend/lastbackend/pkg/client/cmd/user"
+	"github.com/lastbackend/lastbackend/pkg/client/context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestWhoami_Success(t *testing.T) {
 
-	//const (
-	//	username string = "mock"
-	//	email    string = "mock@lastbackend.com"
-	//)
-	//
-	//var ctx = context.Mock()
-	//
-	//token := struct {
-	//	Token string `json:"token"`
-	//}{"mocktoken"}
-	//err := ctx.Storage.Set("session", token)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//
-	//server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//
-	//	tk := r.Header.Get("Authorization")
-	//	assert.NotEmpty(t, tk, "token should be not empty")
-	//	assert.Equal(t, tk, "Bearer "+token.Token, "they should be equal")
-	//
-	//	w.WriteHeader(200)
-	//	_, err := w.Write([]byte(`{"id":"mock", "username":"` + username + `", "email":"` + email + `"}`))
-	//	if err != nil {
-	//		t.Error(err)
-	//		return
-	//	}
-	//}))
-	//defer server.Close()
-	//
-	//ctx.HTTP = h.New(server.URL)
-	//
-	//err = user.Whoami()
-	//if err != nil {
-	//	t.Error(err)
-	//}
+	const (
+		username string = "mock"
+		email    string = "mock@lastbackend.com"
+	)
+
+  var (
+    err error
+    ctx = context.Mock()
+  )
+
+  err = ctx.Storage.Init()
+  if err != nil {
+    panic(err)
+  }
+  defer ctx.Storage.Close()
+
+	token := struct {
+		Token string `json:"token"`
+	}{"mocktoken"}
+
+	err = ctx.Storage.Set("session", token)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tk := r.Header.Get("Authorization")
+		assert.NotEmpty(t, tk, "token should be not empty")
+		assert.Equal(t, tk, "Bearer "+token.Token, "they should be equal")
+
+		w.WriteHeader(200)
+		_, err := w.Write([]byte(`{"id":"mock", "username":"` + username + `", "email":"` + email + `"}`))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}))
+	defer server.Close()
+
+	ctx.HTTP = h.New(server.URL)
+
+	err = user.Whoami()
+	if err != nil {
+		t.Error(err)
+	}
 }
