733592337538b99950d2d2a6ca9f515d66ec6f95
Konstantin Pastor <pastor.konstantin@gmail.com>
2016-12-01 20:47:08 +0300

    create secret

--- pkg/deployer/deployer.go
+++ pkg/deployer/deployer.go
@@ -1,11 +1,104 @@
 package deployer
 
 import (
+	e "github.com/lastbackend/lastbackend/libs/errors"
 	"github.com/lastbackend/lastbackend/libs/model"
+	"github.com/lastbackend/lastbackend/pkg/daemon/context"
 )
 
-type Deployer struct{}
+var deployer *Deployer
 
-func (Deployer) DeployFromTemplate(service string, template *model.Template) error {
+type Deployer struct {
+	ctx *context.Context
+}
+
+func Get() *Deployer {
+
+	if deployer == nil {
+		deployer = &Deployer{
+			ctx: context.Get(),
+		}
+	}
+
+	return deployer
+}
+
+func (d *Deployer) DeployFromTemplate(userID, serviceID string, tpl model.Template) *e.Err {
+
+	var (
+		er  error
+		ctx = d.ctx
+	)
+
+	service, err := ctx.Storage.Service().GetByID(userID, serviceID)
+	if err != nil {
+		ctx.Log.Error(err.Err())
+		return err
+	}
+
+	for _, val := range tpl.Secrets {
+		_, er := ctx.K8S.Core().Secrets(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.Services {
+		_, er := ctx.K8S.Core().Services(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.PersistentVolumes {
+		_, er = ctx.K8S.Core().PersistentVolumes().Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.PersistentVolumeClaims {
+		_, er = ctx.K8S.Core().PersistentVolumeClaims(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.ServiceAccounts {
+		_, er = ctx.K8S.Core().ServiceAccounts(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.Deployments {
+		_, er = ctx.K8S.Extensions().Deployments(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.ReplicationControllers {
+		_, er = ctx.K8S.Core().ReplicationControllers(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
+	for _, val := range tpl.Pods {
+		_, er = ctx.K8S.Core().Pods(service.Project).Create(&val)
+		if er != nil {
+			ctx.Log.Error(er.Error())
+			return e.Service.Unknown(er)
+		}
+	}
+
 	return nil
 }
