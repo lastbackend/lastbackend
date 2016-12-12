36eebb0aafba15620346fa122b7090695630c0ff
Konstantin Pastor <pastor.konstantin@gmail.com>
2016-12-12 14:27:24 +0300

    Update service.go

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,1 +1,106 @@
 package service
+
+import (
+	"errors"
+	"fmt"
+	e "github.com/lastbackend/lastbackend/libs/errors"
+	"github.com/lastbackend/lastbackend/libs/model"
+	"github.com/lastbackend/lastbackend/pkg/daemon/context"
+	"github.com/lastbackend/lastbackend/pkg/util/generator"
+	"k8s.io/client-go/1.5/pkg/api/v1"
+	"k8s.io/client-go/1.5/pkg/apis/extensions/v1beta1"
+)
+
+type Service struct {
+	Name      string            `json:"name"`
+	Namespace string            `json:"namespace"`
+	Labels    map[string]string `json:"labels"`
+	Replicas  int32             `json:"replicas"`
+	config    *v1beta1.Deployment
+}
+
+type ServiceList []Service
+
+func Create(user, project string, config interface{}) (*Service, *e.Err) {
+
+	var (
+		ctx     = context.Get()
+		s       = new(Service)
+		service = new(model.Service)
+	)
+
+	switch config.(type) {
+	case *v1beta1.Deployment:
+		s.config = config.(*v1beta1.Deployment)
+	case *v1.ReplicationController:
+		s.config = convertReplicationControllerToDeployment(config.(*v1.ReplicationController))
+	case *v1.Pod:
+		s.config = convertPodToDeployment(config.(*v1.Pod))
+	default:
+		return nil, e.New("service").Unknown(errors.New("unknown type config"))
+	}
+
+	service.User = user
+	service.Project = project
+	service.Name = fmt.Sprintf("%s-%s", s.config.Name, generator.GetUUIDV4()[0:12])
+
+	service, err := ctx.Storage.Service().Insert(service)
+	if err != nil {
+		return nil, err
+	}
+
+	s.config.Name = service.Name
+
+	return s, nil
+}
+
+func (s Service) Deploy(namespace string) *e.Err {
+
+	var (
+		er  error
+		ctx = context.Get()
+	)
+
+	_, er = ctx.K8S.Extensions().Deployments(namespace).Create(s.config)
+	if er != nil {
+		return e.New("service").Unknown(er)
+	}
+
+	return nil
+}
+
+func convertReplicationControllerToDeployment(config *v1.ReplicationController) *v1beta1.Deployment {
+
+	var deployment = new(v1beta1.Deployment)
+
+	deployment.APIVersion = "extensions/v1beta1"
+	deployment.Kind = "Deployment"
+	deployment.ObjectMeta = config.ObjectMeta
+	deployment.Spec.Strategy.Type = "Recreate"
+	deployment.Spec.Replicas = config.Spec.Replicas
+	deployment.Spec.Template.Spec = config.Spec.Template.Spec
+	deployment.Spec.Template.ObjectMeta = config.Spec.Template.ObjectMeta
+
+	for key, val := range config.Spec.Selector {
+		deployment.Spec.Selector.MatchLabels[key] = val
+	}
+
+	return deployment
+}
+
+func convertPodToDeployment(config *v1.Pod) *v1beta1.Deployment {
+
+	var (
+		replicas   int32 = 1
+		deployment       = new(v1beta1.Deployment)
+	)
+
+	deployment.APIVersion = "extensions/v1beta1"
+	deployment.Kind = "Deployment"
+	deployment.ObjectMeta = config.ObjectMeta
+	deployment.Spec.Strategy.Type = "Recreate"
+	deployment.Spec.Replicas = &replicas
+	deployment.Spec.Template.Spec = config.Spec
+
+	return deployment
+}
