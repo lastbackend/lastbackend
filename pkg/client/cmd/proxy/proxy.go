ccfcf08a0752ce8ad9e6de05415694371ae004d0
lavr <lavrentev.srg@gmail.com>
2017-01-14 13:06:33 +0300

    change proxy structure

--- pkg/client/cmd/proxy/proxy.go
+++ pkg/client/cmd/proxy/proxy.go
@@ -1,15 +1,19 @@
 package proxy
 
 import (
-	"github.com/lastbackend/lastbackend/pkg/proxy/client"
-	p "github.com/lastbackend/lastbackend/pkg/proxy/server/proxy"
-	ps "github.com/lastbackend/lastbackend/pkg/proxy/server/server"
-	"time"
+	ps "github.com/lastbackend/lastbackend/pkg/proxy/server"
+	p "github.com/lastbackend/lastbackend/pkg/proxy/stream"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func Proxy(port string) {
 	go p.StartProxy(port)
 	go ps.StartProxyServer()
-	time.Sleep(1 * time.Millisecond)
-	client.StartProxyClient()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
 }
