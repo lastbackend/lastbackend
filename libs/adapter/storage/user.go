910bc7591d06753f7a588eab867867e3d4e5cd73
unloop <pastor.konstantin@gmail.com>
2017-03-24 18:49:53 +0300

    add filter key for list method in storage

--- libs/adapter/storage/user.go
+++ libs/adapter/storage/user.go
@@ -175,7 +175,7 @@ func (s *UserStorage) SetEmail(username, email string, asDefault bool) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := client.List(ctx, keyList, &list); err != nil {
+	if err := client.List(ctx, keyList, ``, &list); err != nil {
 		return err
 	}
 
