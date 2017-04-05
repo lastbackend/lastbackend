a2ca54c4c838ebac5b55e81e786acc651be29f61
unloop <pastor.konstantin@gmail.com>
2017-04-05 12:38:33 +0300

    fix syslog hook

--- pkg/logger/hooks/os/unix.go
+++ pkg/logger/hooks/os/unix.go
@@ -3,16 +3,18 @@
 package os
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/Sirupsen/logrus/hooks/syslog"
 	"log/syslog"
 )
 
 var hook *logrus_syslog.SyslogHook
 
-func SyslogHook(network, raddr string, tag string) (*logrus_syslog.SyslogHook, error) {
+func SyslogHook(entry *logrus.Entry, network, raddr, tag string) error {
 	var err error
 	if hook == nil {
 		hook, err = logrus_syslog.NewSyslogHook(network, raddr, syslog.LOG_DEBUG, tag)
+		entry.Logger.Hooks.Add(hook)
 	}
-	return hook, err
+	return err
 }
