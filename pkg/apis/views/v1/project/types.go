907e053044d01501ec9cc4da95a5b4ca118ec46d
undassa <alexander@lastbackend.com>
2017-04-09 20:20:45 +0300

    Fix project logic

--- pkg/apis/views/v1/project/types.go
+++ pkg/apis/views/v1/project/types.go
@@ -19,12 +19,12 @@
 package project
 
 import (
-	"time"
 	"github.com/satori/go.uuid"
+	"time"
 )
 
 type Project struct {
-	ID        uuid.UUID `json:"id"`
+	ID          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	Created     time.Time `json:"created"`
