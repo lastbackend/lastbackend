5efb83c521c8922fab1e4cb3b921d876fcc2625c
unloop <pastor.konstantin@gmail.com>
2017-04-14 19:41:00 +0300

    disable debug for etcd

--- pkg/daemon/server/daemon.go
+++ pkg/daemon/server/daemon.go
@@ -24,7 +24,6 @@ import (
 	"github.com/lastbackend/lastbackend/pkg/daemon/config"
 	"github.com/lastbackend/lastbackend/pkg/daemon/context"
 	"github.com/lastbackend/lastbackend/pkg/daemon/storage"
-	"github.com/lastbackend/lastbackend/pkg/daemon/storage/etcd3"
 	"github.com/lastbackend/lastbackend/pkg/logger"
 	"github.com/lastbackend/lastbackend/pkg/util/http"
 	"github.com/lastbackend/lastbackend/pkg/wss"
@@ -118,9 +117,6 @@ func Daemon(cmd *cli.Cmd) {
 		strg, err := storage.Get(cfg.GetEtcdDB())
 		if err != nil {
 			panic(err)
-		}
-		if cfg.Debug {
-			etcd3.SetDebug()
 		}
 		ctx.SetStorage(strg)
 
