4dd1d27d8943ae7a5121fb93d87dc82d2f81a11b
undassa <alexander@lastbackend.com>
2017-05-11 17:28:38 +0800

    Merge remote-tracking branch 'upstream/unstable' into unstable

--- pkg/agent/runtime/cri/docker/container.go
+++ pkg/agent/runtime/cri/docker/container.go
@@ -24,6 +24,7 @@ import (
 	"github.com/lastbackend/lastbackend/pkg/common/types"
 	"strings"
 	"time"
+	"io"
 )
 
 func (r *Runtime) ContainerCreate(ctx context.Context, spec *types.ContainerSpec) (string, error) {
