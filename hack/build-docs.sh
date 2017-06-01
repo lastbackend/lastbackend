910af5797d2c2cfc1b8791b74ea46913a586a2b3
Kirill Merkushev <lanwen@yandex.ru>
2017-06-01 23:44:47 +0300

    remove /latest/ from site's path and update readme

--- hack/build-docs.sh
+++ hack/build-docs.sh
@@ -2,17 +2,18 @@
 
 WORKDIR=${TRAVIS_BUILD_DIR:-`pwd`}
 TAG=latest
+SUBDIR=""
 
 echo "Deleting old output"
 rm -rf ${WORKDIR}/docs/output
-mkdir -p ${WORKDIR}/docs/output/${TAG}
+mkdir -p ${WORKDIR}/docs/output/${SUBDIR}
 
 echo "Copying images"
-cp -R ${WORKDIR}/docs/assets ${WORKDIR}/docs/output/${TAG}/assets
+cp -R ${WORKDIR}/docs/assets ${WORKDIR}/docs/output/${SUBDIR}assets
 
 echo "Copying files"
 cp -R ${WORKDIR}/docs/files/* ${WORKDIR}/docs/output/
 
 echo "Generating docs"
-docker run -v ${WORKDIR}/docs/:/documents/ --name asciidoc-to-html --rm asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAG} -D /documents/output/${TAG} index.adoc
+docker run -v ${WORKDIR}/docs/:/documents/ --name asciidoc-to-html --rm asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAG} -D /documents/output/${SUBDIR} index.adoc
 
