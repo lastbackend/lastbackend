7bec17fb3dec641f662358befc9a5fe3a4bd5525
undassa <undassa@lastbackend.com>
2018-03-16 19:37:41 +0300

    fix node services

--- pkg/node/runtime/node/node.go
+++ pkg/node/runtime/node/node.go
@@ -63,6 +63,7 @@ func GetState() types.NodeState {
 	state.Capacity = GetCapacity()
 	state.Allocated = GetAllocation()
 
+
 	//state.Services.Router.Enabled = viper.GetBool("node.services.router.enabled")
 	//state.Services.Router.ExternalIP = viper.GetString("node.services.router.external_ip")
 	//state.Services.Builder = viper.GetBool("node.services.builder")
