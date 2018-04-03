fe1e58ccf66ea97424c739602901915c2a228812
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2018-04-03 17:22:25 +0300

    Update build-docs.sh

--- hack/build-docs.sh
+++ hack/build-docs.sh
@@ -5,14 +5,14 @@ TAG=latest
 
 echo "Deleting old output"
 rm -rf ${WORKDIR}/docs/output
-mkdir -p ${WORKDIR}/docs/output/${TAG}
+mkdir -p ${WORKDIR}/docs/output
 
 echo "Copying images"
-cp -R ${WORKDIR}/docs/assets ${WORKDIR}/docs/output/${TAG}/assets
+cp -R ${WORKDIR}/docs/assets ${WORKDIR}/docs/output/assets
 
 echo "Copying files"
 cp -R ${WORKDIR}/docs/files/* ${WORKDIR}/docs/output/
 
 echo "Generating docs"
-docker run -v ${WORKDIR}/docs/:/documents/ --name asciidoc-to-html --rm asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAG} -D /documents/output/${TAG} index.adoc
+docker run -v ${WORKDIR}/docs/:/documents/ --name asciidoc-to-html --rm asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAG} -D /documents/output index.adoc
 
