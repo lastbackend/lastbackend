8cbb00a39dcbddb4411c537053b89755f3ec4299
undassa <undassa@lastbackend.com>
2018-04-17 20:35:00 +0300

    update makefile

--- hack/build-images.sh
+++ hack/build-images.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t lastbackend/lastbackend -f ./images/lastbackend/Dockerfile .
+if [ $1 == "ingress" ] || [ $1 == ""] ; then
+  docker build -t lastbackend/ingress -f ./images/ingress/Dockerfile .
+fi
+
+if [ $1 == "lastbackend" ] || [ $1 == "" ] ; then
+  docker build -t lastbackend/lastbackend -f ./images/lastbackend/Dockerfile .
+fi
