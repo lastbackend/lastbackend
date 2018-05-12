76a98e4678ce0feda3a6fead098ab4c6cbb50132
unloop <pastor.konstantin@gmail.com>
2018-05-12 21:43:09 +0300

    update view for node handler

--- pkg/api/types/v1/views/node_utils.go
+++ pkg/api/types/v1/views/node_utils.go
@@ -36,10 +36,10 @@ func (nv *NodeView) New(obj *types.Node) *Node {
 
 func (nv *NodeView) ToNodeMeta(meta types.NodeMeta) NodeMeta {
 	return NodeMeta{
-		Name:        meta.Name,
-		Description: meta.Description,
-		Created:     meta.Created,
-		Updated:     meta.Updated,
+		Name:     meta.Name,
+		SelfLink: meta.SelfLink,
+		Created:  meta.Created,
+		Updated:  meta.Updated,
 	}
 }
 
