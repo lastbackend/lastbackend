603a6c53da6b577251d0e6a8c6cd9f40520c607b
unloop <pastor.konstantin@gmail.com>
2018-08-31 11:30:05 +0300

    remove cli and change makefile

--- pkg/util/filesystem/filesystem.go
+++ pkg/util/filesystem/filesystem.go
@@ -71,3 +71,22 @@ func ReadFile(path string) (value []byte, err error) {
 	}
 	return value, nil
 }
+
+// HomeDir returns the home directory for the current user
+func HomeDir() string {
+	if runtime.GOOS == "windows" {
+		if homeDrive, homePath := os.Getenv("HOMEDRIVE"), os.Getenv("HOMEPATH"); len(homeDrive) > 0 && len(homePath) > 0 {
+			homeDir := homeDrive + homePath
+			if _, err := os.Stat(homeDir); err == nil {
+				return homeDir
+			}
+		}
+		if userProfile := os.Getenv("USERPROFILE"); len(userProfile) > 0 {
+			if _, err := os.Stat(userProfile); err == nil {
+				return userProfile
+			}
+		}
+	}
+	return os.Getenv("HOME")
+}
+
