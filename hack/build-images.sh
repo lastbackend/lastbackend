eaad4eb3575ca9f65fc58d18db3d34e3aa7aebad
undassa <alexander@lastbackend.com>
2019-02-13 21:53:30 +0300

    ns:apply

--- hack/build-images.sh
+++ hack/build-images.sh
@@ -9,6 +9,6 @@ fi
 ## now loop through the components array
 for i in "${arr[@]}"
 do
- echo "Build '$i' version '$VERSION' for os '$OSTYPE'"
- docker build -t "index.lstbknd.net/lastbackend/$i" -f "./images/&i/Dockerfile" .
+ echo "Build '$i' version '$VERSION'"
+ docker build -t "index.lstbknd.net/lastbackend/$i" -f "./images/$i/Dockerfile" .
 done
